65d06ae8ac7d98081d6ded9143b4b096b239c123
Lorena Guranda <lorena1985@ymail.com>
2023-04-28 22:48:14 +0000

    Add variables to the DOM

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,11 @@
-console.log(hello);
+const userScore = 0;
+const computerScore = 0;
+const userScore_span = document.getElementById("user-score");
+const computerScore_span = document.getElementById("computer-score");
+const scoreBoard_div = document.querySelector(".score-board");
+const result_div = document.querySelector(".result");
+const rock_div = document.getElementById("rock");
+const paper_div = document.getElementById("paper");
+const scissors_div = document.getElementById("scissors");
+const lizard_div = document.getElementById("lizard");
+const spock_div = document.getElementById("spock");
